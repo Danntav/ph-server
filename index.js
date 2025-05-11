5fe607e05f37c3d9beb8e2d88c21bf48bacdc0cb
Daniel Tavares <121188402+Danntav@users.noreply.github.com>
2025-05-11 18:43:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { Pool } = require('pg');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
